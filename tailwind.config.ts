7d37201d86a69d514d73d6ec7938c472f81668a4
Elyas Asmad <elyasasmadz@gmail.com>
2023-02-06 13:21:33 +0800

    🔧 Other configs

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default <Partial<Config>>{
+    theme: {
+        fontFamily: {
+            sans: [
+                'Inter',
+                'sans-serif'
+            ]
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'gold': '#cda936'
+        },
+        extend: {
+            animation: {
+                'card-float': 'float 3s ease-in-out infinite'
+            },
+            keyframes: {
+                float: {
+                    '0%, 100%': {
+                        boxShadow: '0 5px 15px 0px rgba(0,0,0,0.6)',
+                        transform: 'translateY(0px)'
+                    },
+                    '50%': {
+                        boxShadow: '0 25px 15px 0px rgba(0,0,0,0.2)',
+                        transform: 'translateY(-20px)'
+                    }
+                }
+            }
+        }
+    },
+};
