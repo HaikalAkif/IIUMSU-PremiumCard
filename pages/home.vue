b85177c89c7ea219b7b44837a086c1bc15985209
Elyas Asmad <elyasasmadz@gmail.com>
2023-02-10 23:58:24 +0800

    ✨ Homepage redesign

---
+++ pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+            <NuxtLink>
+                <img src="~/assets/images/su-removebg-preview.png" class="h-6 mr-3 sm:h-9" alt="">
+            </NuxtLink>
+            <button class="inline-flex items-center p-2 ml-3 transition-all duration-300 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <span class="sr-only">Menu</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+        </div>
+    </nav>
+</template>
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
